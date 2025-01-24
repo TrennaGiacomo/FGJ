f2f56d5b629017cc24fc6990fa61687fce461134
Giacomo Trenna <ae2342@student.jamk.fi>
2025-01-24 22:22:40 +0200

    Made DoTween floating Anim, and added Bounds

--- FGJ2025/Assets/DraggableBubble.cs
+++ FGJ2025/Assets/DraggableBubble.cs
@@ -1,34 +1,36 @@
 using UnityEngine;
 
-public class DragSprite : MonoBehaviour
+public class DraggableBubble : MonoBehaviour
 {
     private Vector3 offset;
     private Camera mainCamera;
 
+    public bool isDragging = false;
+
     void Start()
     {
-        // Get the main camera
         mainCamera = Camera.main;
     }
 
     void OnMouseDown()
     {
-        // Calculate the offset between the sprite and the mouse position in world space
+        isDragging = true;
         offset = transform.position - GetMouseWorldPosition();
     }
 
     void OnMouseDrag()
     {
-        // Update the sprite position to follow the mouse
         transform.position = GetMouseWorldPosition() + offset;
+    }
+    void OnMouseUp()
+    {
+        isDragging = false;
     }
 
     private Vector3 GetMouseWorldPosition()
     {
-        // Get the mouse position in screen space
         Vector3 mouseScreenPosition = Input.mousePosition;
-        // Convert mouse position to world space, assuming the sprite is on the Z = 0 plane
-        mouseScreenPosition.z = Mathf.Abs(mainCamera.transform.position.z); // Adjust for camera depth
+        mouseScreenPosition.z = Mathf.Abs(mainCamera.transform.position.z);
         return mainCamera.ScreenToWorldPoint(mouseScreenPosition);
     }
 }
